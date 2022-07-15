a003d512f82c8ba2cb07b57773044b181f948984
Emad Jacob Maroun <emoun.open@gmail.com>
2022-07-15 14:50:02 +0200

    Implemented wrapping increment instruction.

--- src/execution.rs
+++ src/execution.rs
@@ -3,7 +3,7 @@ use crate::{
 	MetricTracker, Scalar, ValueType,
 };
 use byteorder::ByteOrder;
-use scry_isa::{AluVariant, CallVariant, Instruction};
+use scry_isa::{AluVariant, Bits, CallVariant, Instruction};
 use std::fmt::Debug;
 
 /// The result of performing one execution step
@@ -113,83 +113,9 @@ impl Executor
 					// Discard ready queue
 					let _ = self.operands.ready_iter(&mut self.memory, tracker);
 				},
-				Alu(AluVariant::Add, offset) =>
+				Alu(variant, offset) =>
 				{
-					let (typ, mut result_scalars) = {
-						// Extract operands
-						let mut ins = self.operands.ready_iter(&mut self.memory, tracker);
-						let in1 = ins.next().unwrap();
-						let typ = in1.0.value_type();
-						let in1 = in1.0.iter();
-						let in2 = ins.next().unwrap();
-						let in2 = in2.0.iter();
-
-						let mut result_scalars = Vec::new();
-						for (sc1, sc2) in in1.zip(in2)
-						{
-							let mut result_bytes = Vec::new();
-							let mut carry = false;
-
-							for (b1, b2) in
-								sc1.bytes().unwrap().iter().zip(sc2.bytes().unwrap().iter())
-							{
-								fn carrying_add(v1: u8, v2: u8, c: bool) -> (u8, bool)
-								{
-									let r = (v1 as u16) + (v2 as u16) + (c as u16);
-									let carry = r > u8::MAX as u16;
-									(r as u8, carry)
-								}
-								let (r, c) = carrying_add(*b1, *b2, carry);
-								result_bytes.push(r);
-								carry = c;
-							}
-
-							// If overflow, saturate
-							match (typ, carry)
-							{
-								(ValueType::Uint(_), true) =>
-								{
-									result_bytes.iter_mut().for_each(|b| *b = u8::MAX)
-								},
-								(ValueType::Int(_), _) =>
-								{
-									// If the input both have the same sign (both positive or both
-									// negative), then overflow occurs if and only if the result has
-									// the opposite sign.
-									// Source: https://www.doc.ic.ac.uk/~eedwards/compsys/arithmetic/index.html (2022-07-14)
-									let signed_1 =
-										sc1.bytes().unwrap().last().unwrap() & 0b10000000 != 0;
-									let signed_2 =
-										sc2.bytes().unwrap().last().unwrap() & 0b10000000 != 0;
-									let signed_result =
-										result_bytes.last().unwrap() & 0b10000000 != 0;
-
-									if signed_1 && signed_2 && !signed_result
-									{
-										// underflow, set to lowest negative
-										result_bytes.iter_mut().for_each(|b| *b = 0);
-										*result_bytes.last_mut().unwrap() = 0b10000000u8;
-									}
-									else if !signed_1 && !signed_2 && signed_result
-									{
-										// overflow, set to highest value
-										result_bytes.iter_mut().for_each(|b| *b = u8::MAX);
-										*result_bytes.last_mut().unwrap() = 0b01111111u8;
-									}
-								},
-								_ => (),
-							}
-							result_scalars.push(Scalar::Val(result_bytes.into_boxed_slice()));
-						}
-						(typ, result_scalars)
-					};
-					self.operands.push_operand(
-						offset.value as usize,
-						Value::new_typed(typ, result_scalars.remove(0), result_scalars)
-							.unwrap()
-							.into(),
-						tracker,
-					);
+					self.perform_alu(variant, offset, tracker);
 				},
 				_ => todo!(),
 			}
@@ -211,5 +137,155 @@ impl Executor
 					.into_iter(),
 			)
 		}
+	}
+
+	/// Executes an Alu instruction, consuming the needed inputs and putting the
+	/// result in the relevant queue.
+	///
+	/// The given variant is the Alu instruction to execute with the offset
+	/// being which queue the result should go to (0 means the first queue after
+	/// the queue of the inputs has been discarded).
+	fn perform_alu(
+		&mut self,
+		variant: AluVariant,
+		offset: Bits<5, false>,
+		tracker: &mut impl MetricTracker,
+	)
+	{
+		let (typ, mut result_scalars) = {
+			let ins_count = match variant
+			{
+				AluVariant::Add => 2,
+				AluVariant::Inc => 1,
+				_ => todo!(),
+			};
+
+			// Extract operands
+			let mut ins = self.operands.ready_iter(&mut self.memory, tracker);
+			let mut result_scalars = Vec::new();
+			let in1 = ins.next().unwrap();
+			let typ = in1.0.value_type();
+			let in1 = in1.0.iter();
+
+			if ins_count == 2
+			{
+				let in2 = ins.next().unwrap();
+				let in2 = in2.0.iter();
+
+				for (sc1, sc2) in in1.zip(in2)
+				{
+					result_scalars.push(Scalar::Val(
+						Self::alu_add_saturated(sc1, sc2, typ).into_boxed_slice(),
+					));
+				}
+			}
+			else if ins_count == 1
+			{
+				for sc1 in in1
+				{
+					result_scalars.push(Scalar::Val(
+						Self::alu_increment_wrapping(sc1, typ).into_boxed_slice(),
+					));
+				}
+			}
+			else
+			{
+				todo!()
+			}
+			(typ, result_scalars)
+		};
+		self.operands.push_operand(
+			offset.value as usize,
+			Value::new_typed(typ, result_scalars.remove(0), result_scalars)
+				.unwrap()
+				.into(),
+			tracker,
+		);
+	}
+
+	/// Performs a byte-wise addition with carry.
+	///
+	/// Returns the resulting bytes and whether the final carry was set.
+	///
+	/// Assumes the two given scalars are valid values (not Nan or Nar) and have
+	/// the same length. The result then has the same length.
+	fn alu_add_carry(in1: &Scalar, in2: &Scalar) -> (Vec<u8>, bool)
+	{
+		let mut result_bytes = Vec::new();
+		let mut carry = false;
+
+		for (b1, b2) in in1.bytes().unwrap().iter().zip(in2.bytes().unwrap().iter())
+		{
+			fn carrying_add(v1: u8, v2: u8, c: bool) -> (u8, bool)
+			{
+				let r = (v1 as u16) + (v2 as u16) + (c as u16);
+				let carry = r > u8::MAX as u16;
+				(r as u8, carry)
+			}
+			let (r, c) = carrying_add(*b1, *b2, carry);
+			result_bytes.push(r);
+			carry = c;
+		}
+
+		(result_bytes, carry)
+	}
+
+	/// Performs a saturated addition on the given scalars.
+	///
+	/// The bytes of the inputs are assumed to have the given type.
+	///
+	/// Returns the resulting bytes. Assumes the two given scalars are valid
+	/// values (not Nan or Nar) and have the same length. The result then has
+	/// the same length.
+	fn alu_add_saturated(in1: &Scalar, in2: &Scalar, typ: ValueType) -> Vec<u8>
+	{
+		let (mut result_bytes, carry) = Self::alu_add_carry(in1, in2);
+
+		// If overflow, saturate
+		match (typ, carry)
+		{
+			(ValueType::Uint(_), true) => result_bytes.iter_mut().for_each(|b| *b = u8::MAX),
+			(ValueType::Int(_), _) =>
+			{
+				// If the input both have the same sign (both positive or both
+				// negative), then overflow occurs if and only if the result has
+				// the opposite sign.
+				// Source: https://www.doc.ic.ac.uk/~eedwards/compsys/arithmetic/index.html (2022-07-14)
+				let signed_1 = in1.bytes().unwrap().last().unwrap() & 0b10000000 != 0;
+				let signed_2 = in2.bytes().unwrap().last().unwrap() & 0b10000000 != 0;
+				let signed_result = result_bytes.last().unwrap() & 0b10000000 != 0;
+
+				if signed_1 && signed_2 && !signed_result
+				{
+					// underflow, set to lowest negative
+					result_bytes.iter_mut().for_each(|b| *b = 0);
+					*result_bytes.last_mut().unwrap() = 0b10000000u8;
+				}
+				else if !signed_1 && !signed_2 && signed_result
+				{
+					// overflow, set to highest value
+					result_bytes.iter_mut().for_each(|b| *b = u8::MAX);
+					*result_bytes.last_mut().unwrap() = 0b01111111u8;
+				}
+			},
+			_ => (),
+		}
+		result_bytes
+	}
+
+	/// Performs a wrapping addition of the given scalar and 1.
+	///
+	/// The bytes of the input are assumed to have the given type.
+	///
+	/// Returns the resulting bytes of the same length as the input.
+	fn alu_increment_wrapping(in1: &Scalar, typ: ValueType) -> Vec<u8>
+	{
+		// Create a scalar with value 1 to add to the given scalar
+		let mut bytes = Vec::with_capacity(typ.scale());
+		bytes.push(1);
+		bytes.resize(typ.scale(), 0);
+		let one_scalar = Scalar::Val(bytes.into_boxed_slice());
+
+		Self::alu_add_carry(in1, &one_scalar).0
 	}
 }
--- tests/executor/alu_instructions.rs
+++ tests/executor/alu_instructions.rs
@@ -8,32 +8,71 @@ use scryer::{
 	ExecState, Metric, OperandState, Scalar, TrackReport, Value,
 };
 
-fn calculate_result<T>(
-	sc1: &Scalar,
-	sc2: &Scalar,
-	semantic_fn: impl Fn(T, T) -> T,
+/// Manages the calculation of applying the given semantic function to the given
+/// inputs.
+///
+/// Returns the result of the semantic function as a scalar.
+///
+/// `read` must be able to convert a u8 slice into the type that will be used
+/// for the calculation. `write` must be able to write the result of the
+/// calculation into a u8 slice.
+fn calculate_result<T: Default + Copy, const OPS: usize>(
+	inputs: [&Scalar; OPS],
+	semantic_fn: impl Fn([T; OPS]) -> T,
 	read: impl Fn(&[u8]) -> T,
 	write: impl Fn(&mut [u8], T),
 ) -> Scalar
 {
-	let v1 = read(sc1.bytes().unwrap());
-	let v2 = read(sc2.bytes().unwrap());
-	let r = semantic_fn(v1, v2);
+	let len = inputs[0].bytes().unwrap().len();
+	let mut values = [T::default(); OPS];
+	values
+		.iter_mut()
+		.zip(inputs.into_iter())
+		.for_each(|(v, sc)| *v = read(sc.bytes().unwrap()));
+	let r = semantic_fn(values);
 	let mut r_bytes = Vec::new();
-	r_bytes.resize(sc1.bytes().unwrap().len(), 0);
+	r_bytes.resize(len, 0);
 	write(r_bytes.as_mut_slice(), r);
 	Scalar::Val(r_bytes.into_boxed_slice())
 }
 
-#[quickcheck]
-fn add(
-	// RestrictedExecState(state): RestrictedExecState<false, false, 2, 2, true>,
-	state: NoCF<SimpleOps<NoReads<LimitedOps<ExecState, 2, 2>>>>,
+/// The restricted execution state we will use for generating states for all Alu
+/// instruction tests.
+///
+/// We only test the instructions with the specific number of inputs they
+/// consume and without any operands being Nan, Nar, nor needing to read from
+/// memory. Since all the other cases need to be handled in the same way, they
+/// will be tested in a way that is agnostic to the specific instruction variant
+type AluTestState<const OPS: usize> = NoCF<SimpleOps<NoReads<LimitedOps<ExecState, OPS, OPS>>>>;
+
+/// Tests the given Alu instruction variant on the given state with the given
+/// offset in the instruction.
+///
+/// `OPS` must match the exact number of inputs that given instruction variant
+/// consumes. `*_sem` is a semantic function for each type of integer that can
+/// be used to check whether the instruction has performed correctly.
+/// So they are essentially the golden model.
+///
+/// Tests both the resulting state after taking 1 step and that the reported
+/// metrics are correct
+fn test_alu_instruction<const OPS: usize>(
+	state: AluTestState<OPS>,
 	offset: Bits<5, false>,
+	variant: AluVariant,
+	u8_sem: impl Fn([u8; OPS]) -> u8,
+	u16_sem: impl Fn([u16; OPS]) -> u16,
+	u32_sem: impl Fn([u32; OPS]) -> u32,
+	u64_sem: impl Fn([u64; OPS]) -> u64,
+	u128_sem: impl Fn([u128; OPS]) -> u128,
+	i8_sem: impl Fn([i8; OPS]) -> i8,
+	i16_sem: impl Fn([i16; OPS]) -> i16,
+	i32_sem: impl Fn([i32; OPS]) -> i32,
+	i64_sem: impl Fn([i64; OPS]) -> i64,
+	i128_sem: impl Fn([i128; OPS]) -> i128,
 ) -> TestResult
 {
 	let state = state.0 .0 .0 .0;
-	state.valid();
+
 	if state
 		.frame
 		.op_queues
@@ -45,156 +84,154 @@ fn add(
 		return TestResult::discard();
 	}
 
-	let mut nop_encoded = [0; 2];
-	LittleEndian::write_u16(
-		&mut nop_encoded,
-		Instruction::Alu(AluVariant::Add, offset).encode(),
-	);
-	let exec = Executor::from_state(&state, Memory::new(nop_encoded.into(), state.address));
+	// Encode instruction
+	let mut encoded = [0; 2];
+	LittleEndian::write_u16(&mut encoded, Instruction::Alu(variant, offset).encode());
+
+	// Create execution from state with only access to the instruction
+	let exec = Executor::from_state(&state, Memory::new(encoded.into(), state.address));
 	let mut metrics = TrackReport::new();
+
+	// Perform one step
 	let res = match exec.step(&mut metrics)
 	{
 		ExecResult::Ok(exec) =>
 		{
+			// Calculate expected state after step
 			let mut expected_state = state.clone();
+			// Advance to next instruction
 			expected_state.address += 2;
+
 			let mut new_op_q = state.frame.op_queues.clone();
 			// Remove ready-queue
-			let (rem1, rem2) = new_op_q
-				.remove(&0)
-				.map(|(op1, mut op_rest)| (op1, op_rest.pop().unwrap()))
-				.unwrap();
-			// Reduce all queue indexes by 1
+			let (removed_first, removed_rest) = new_op_q.remove(&0).unwrap();
+			let removed_rest_values: Vec<Value> = removed_rest
+				.into_iter()
+				.map(|op| op.extract_value())
+				.collect();
+
+			// Reduce all operand queue indices by 1
 			expected_state.frame.op_queues = new_op_q
 				.into_iter()
 				.map(|(idx, ops)| (idx - 1, ops))
 				.collect();
 
-			// Calculate expected result
-			let v1 = rem1.extract_value();
-			let v2 = rem2.extract_value();
+			// Calculate expected result operand
 			let mut result_scalars = Vec::new();
-			let typ = v1.value_type();
-			for (sc1, sc2) in v1.iter().zip(v2.iter())
+			let first_value = removed_first.extract_value();
+			let typ = first_value.value_type();
+			for (scalar_idx, sc0) in first_value.iter().enumerate()
 			{
-				use scryer::ValueType::*;
-				match typ
+				let nan = Scalar::Nan;
+				let mut scalars = [&nan; OPS];
+				scalars[0] = sc0;
+				for input_idx in 1..OPS
 				{
-					Uint(0) =>
-					{
-						result_scalars.push(calculate_result(
-							sc1,
-							sc2,
-							u8::saturating_add,
-							|b| b[0],
-							|b, v| b[0] = v,
-						))
-					},
+					scalars[input_idx] = removed_rest_values[input_idx - 1]
+						.iter()
+						.nth(scalar_idx)
+						.unwrap();
+				}
+
+				// Calculate result based on type
+				use scryer::ValueType::*;
+				result_scalars.push(match typ
+				{
+					Uint(0) => calculate_result(scalars, &u8_sem, |b| b[0], |b, v| b[0] = v),
 					Uint(1) =>
 					{
-						result_scalars.push(calculate_result(
-							sc1,
-							sc2,
-							u16::saturating_add,
+						calculate_result(
+							scalars,
+							&u16_sem,
 							LittleEndian::read_u16,
 							LittleEndian::write_u16,
-						))
+						)
 					},
 					Uint(2) =>
 					{
-						result_scalars.push(calculate_result(
-							sc1,
-							sc2,
-							u32::saturating_add,
+						calculate_result(
+							scalars,
+							&u32_sem,
 							LittleEndian::read_u32,
 							LittleEndian::write_u32,
-						))
+						)
 					},
 					Uint(3) =>
 					{
-						result_scalars.push(calculate_result(
-							sc1,
-							sc2,
-							u64::saturating_add,
+						calculate_result(
+							scalars,
+							&u64_sem,
 							LittleEndian::read_u64,
 							LittleEndian::write_u64,
-						))
+						)
 					},
 					Uint(4) =>
 					{
-						result_scalars.push(calculate_result(
-							sc1,
-							sc2,
-							u128::saturating_add,
+						calculate_result(
+							scalars,
+							&u128_sem,
 							LittleEndian::read_u128,
 							LittleEndian::write_u128,
-						))
+						)
 					},
 					Int(0) =>
 					{
-						result_scalars.push(calculate_result(
-							sc1,
-							sc2,
-							i8::saturating_add,
-							|b| b[0] as i8,
-							|b, v| b[0] = v as u8,
-						))
+						calculate_result(scalars, &i8_sem, |b| b[0] as i8, |b, v| b[0] = v as u8)
 					},
 					Int(1) =>
 					{
-						result_scalars.push(calculate_result(
-							sc1,
-							sc2,
-							i16::saturating_add,
+						calculate_result(
+							scalars,
+							&i16_sem,
 							LittleEndian::read_i16,
 							LittleEndian::write_i16,
-						))
+						)
 					},
 					Int(2) =>
 					{
-						result_scalars.push(calculate_result(
-							sc1,
-							sc2,
-							i32::saturating_add,
+						calculate_result(
+							scalars,
+							&i32_sem,
 							LittleEndian::read_i32,
 							LittleEndian::write_i32,
-						))
+						)
 					},
 					Int(3) =>
 					{
-						result_scalars.push(calculate_result(
-							sc1,
-							sc2,
-							i64::saturating_add,
+						calculate_result(
+							scalars,
+							&i64_sem,
 							LittleEndian::read_i64,
 							LittleEndian::write_i64,
-						))
+						)
 					},
 					Int(4) =>
 					{
-						result_scalars.push(calculate_result(
-							sc1,
-							sc2,
-							i128::saturating_add,
+						calculate_result(
+							scalars,
+							&i128_sem,
 							LittleEndian::read_i128,
 							LittleEndian::write_i128,
-						))
+						)
 					},
 					Uint(_) | Int(_) => unreachable!(),
-				}
+				});
 			}
 			let result_value =
 				Value::new_typed(typ, result_scalars.remove(0), result_scalars).unwrap();
 
+			// Put expected result in correct expected state queue
 			if let Some((_, op_rest)) = expected_state
 				.frame
 				.op_queues
 				.get_mut(&(offset.value as usize))
 			{
+				// Push on end of queue
 				op_rest.push(OperandState::Ready(result_value));
 			}
 			else
 			{
+				// No existing queue, create it
 				expected_state.frame.op_queues.insert(
 					offset.value as usize,
 					(OperandState::Ready(result_value), vec![]),
@@ -203,7 +240,11 @@ fn add(
 
 			if exec.state() != expected_state
 			{
-				TestResult::error(format!("{:?} != {:?}", exec.state(), expected_state))
+				TestResult::error(format!(
+					"Unexpected end state (actual != expected):\n{:?} != {:?}",
+					exec.state(),
+					expected_state
+				))
 			}
 			else
 			{
@@ -212,14 +253,17 @@ fn add(
 					(Metric::InstructionReads, 1),
 					(Metric::QueuedValues, 1),
 					(Metric::QueuedValueBytes, typ.scale()),
-					(Metric::ConsumedOperands, 2),
-					(Metric::ConsumedBytes, typ.scale() * 2),
+					(Metric::ConsumedOperands, OPS),
+					(Metric::ConsumedBytes, typ.scale() * OPS),
 				]
 				.into();
 
 				if metrics != expected_mets
 				{
-					TestResult::error(format!("{:?} != {:?}", metrics, expected_mets))
+					TestResult::error(format!(
+						"Unexpected step metrics (actual != expected):\n{:?} != {:?}",
+						metrics, expected_mets
+					))
 				}
 				else
 				{
@@ -227,7 +271,49 @@ fn add(
 				}
 			}
 		},
-		err => TestResult::error(format!("Unexpected: {:?}", err)),
+		err => TestResult::error(format!("Unexpected step result: {:?}", err)),
 	};
 	res
+}
+
+/// Test the Alu instruction variant `Add`
+#[quickcheck]
+fn add(state: AluTestState<2>, offset: Bits<5, false>) -> TestResult
+{
+	test_alu_instruction(
+		state,
+		offset,
+		AluVariant::Add,
+		|sc| u8::saturating_add(sc[0], sc[1]),
+		|sc| u16::saturating_add(sc[0], sc[1]),
+		|sc| u32::saturating_add(sc[0], sc[1]),
+		|sc| u64::saturating_add(sc[0], sc[1]),
+		|sc| u128::saturating_add(sc[0], sc[1]),
+		|sc| i8::saturating_add(sc[0], sc[1]),
+		|sc| i16::saturating_add(sc[0], sc[1]),
+		|sc| i32::saturating_add(sc[0], sc[1]),
+		|sc| i64::saturating_add(sc[0], sc[1]),
+		|sc| i128::saturating_add(sc[0], sc[1]),
+	)
+}
+
+/// Test the Alu instruction variant `Inc`
+#[quickcheck]
+fn inc(state: AluTestState<1>, offset: Bits<5, false>) -> TestResult
+{
+	test_alu_instruction(
+		state,
+		offset,
+		AluVariant::Inc,
+		|sc| u8::wrapping_add(sc[0], 1),
+		|sc| u16::wrapping_add(sc[0], 1),
+		|sc| u32::wrapping_add(sc[0], 1),
+		|sc| u64::wrapping_add(sc[0], 1),
+		|sc| u128::wrapping_add(sc[0], 1),
+		|sc| i8::wrapping_add(sc[0], 1),
+		|sc| i16::wrapping_add(sc[0], 1),
+		|sc| i32::wrapping_add(sc[0], 1),
+		|sc| i64::wrapping_add(sc[0], 1),
+		|sc| i128::wrapping_add(sc[0], 1),
+	)
 }
